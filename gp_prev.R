9eb0b58c72cd96a29e839dde9fdd15050d41a8ae
julianflowers <julianflowers@users.noreply.github.com>
2016-08-08 09:00:39 +0100

    Updated script

--- gp_prev.R
+++ gp_prev.R
@@ -29,6 +29,12 @@ unzip(zipfile = "~/Documents/R_projects/prevalence_estimates/Hypertension-estima
 unzip(zipfile = "~/Documents/R_projects/prevalence_estimates/FW%3a_Stroke_prevalence_model_estimates.zip", 
       exdir = "~/Documents/R_projects/prevalence_estimates")
 
+unzip(zipfile = "~/Documents/R_projects/prevalence_estimates/COPD_prevalence_estimates_&_Technical_Document.zip", 
+      exdir = "~/Documents/R_projects/prevalence_estimates")
+
+unzip(zipfile = "~/Documents/R_projects/prevalence_estimates/COPD-prevalence-estimates.zip", 
+      exdir = "~/Documents/R_projects/prevalence_estimates")
+
 ## GP data - extract, combine, tidy
 
 
@@ -50,7 +56,9 @@ pad1.GP <- read_excel("~/Documents/R_projects/prevalence_estimates/PAD-prevalenc
 
 pad1imd.GP <- read_dta("~/Documents/R_projects/prevalence_estimates/PAD-prevalence estimates/PAD-prevalence-estimates-practice-level+QOF-with-IMD.dta")
 
-list <- list(depress.GP, chd.GP, stroke.GP, dxbp.GP, undxbp.GP, qofbp.GP, stroke1.GP, pad1.GP, pad1imd.GP )
+copd.GP <- read_excel("~/Documents/R_projects/prevalence_estimates/COPD-estimated+QOF-prevalence-practice-level.xlsx")
+
+list <- list(depress.GP, chd.GP, stroke.GP, dxbp.GP, undxbp.GP, qofbp.GP, stroke1.GP, pad1.GP, pad1imd.GP, copd.GP )
 
 
 ## Look at table dimenstions
@@ -71,6 +79,7 @@ l_stroke1.GP <- gather(stroke1.GP, indicator, value, 2:ncol(stroke.GP))
 
 l_pad1.GP <- gather(pad1.GP, indicator, value, 2:ncol(pad1.GP))
 l_padimd1.GP <- gather(pad1imd.GP, indicator, value, 2:ncol(pad1imd.GP))
+l_copd.GP <- gather(copd.GP, indicator, value, 2:ncol(copd.GP))
 
 ## Labels
 
@@ -83,8 +92,9 @@ l_undxbp.GP$disease <- "undx_bp"
 l_qofbp.GP$disease <- "bp"
 l_pad1.GP$disease <- "pad"
 l_padimd1.GP$disease <- "pad_IMD"
+l_copd.GP$disease <- "copd"
 
-list1 <- list(l_depress.GP, l_chd.GP, l_stroke.GP, l_dxbp.GP, l_undxbp.GP, l_qofbp.GP, l_stroke1.GP, l_pad1.GP, l_padimd1.GP)
+list1 <- list(l_depress.GP, l_chd.GP, l_stroke.GP, l_dxbp.GP, l_undxbp.GP, l_qofbp.GP, l_stroke1.GP, l_pad1.GP, l_padimd1.GP, l_copd.GP)
 
 ## Check table dimensions
 sapply(list1, dim)
@@ -110,13 +120,14 @@ s1
 
 gp_all_prev <- gp_all %>%
   mutate(prevalence = ifelse(indicator == "Estprevalence" | indicator == "Estimated prevalence" | indicator == "Prevestimate" | indicator == "EstPrev" | indicator == "Estprev" | indicator == "estimate",1,0)) %>%
-  mutate(qof = ifelse(indicator == "QOFprev" | indicator == "QOFprevaelnce201415"| indicator == "QOFprevalence\r\n" | indicator == "Prevalence\r\n(per cent)", 1, 0)) 
+  mutate(qof = ifelse(indicator == "QOFprev" | indicator == "QOFprevaelnce201415"| indicator == "QOFprevalence\r\n" | indicator == "QOFprevalence" | indicator == "Prevalence\r\n(per cent)", 1, 0)) 
 
 head(gp_all_prev)
 gp_all_prev$index <- with(gp_all_prev, paste(disease, indicator, sep = "."))
 
 
 gp_all_prev1 <- as.data.table(gp_all_prev %>% filter(prevalence == 1)) %>% select(practice_code, index, value)
+gp_all_prev1 <- gp_all_prev1[-(75511:75512),] ## exclude duplicate rows
 
 ## Convert file back to wide format
 wgp <- data.table(spread(gp_all_prev1, index, value))
@@ -125,45 +136,81 @@ wgp <- data.table(spread(gp_all_prev1, index, value))
 
 ## Convert QOF dato wide format
 gp_all_prev2 <- as.data.table(gp_all_prev %>% filter(qof == 1)) %>% select(practice_code, index, value)
+gp_all_prev2 <- gp_all_prev2[-(30366:30367),]
+
 wgp1 <- data.table(spread(gp_all_prev2, index, value))
+
 
 # Join datasets
 setkey(wgp, practice_code)
 setkey(wgp1, practice_code)
 
-DT <- wgp[wgp1]
+df <- wgp[wgp1]
 
 ## NAs
 
-apply(DT, 2,  function(x) mean(is.na(x)))
+apply(df, 2,  function(x) mean(is.na(x)))
 
 ## Simplify column names
 
-colnames(DT) <- c("prac.code", "bp", "bp1", "chd", "depression","pad_IMD", "pad",  "stroke.IMD", "stroke", "bp_notdiag", "bp.qof", "chd.qof", "depression.qof")
+colnames(df) <- c("prac.code", "bp(%)", "bp1", "chd", "copd", "depression","pad_IMD", "pad",  "stroke.IMD", "stroke", "bp_notdiag", "bp.qof", "chd.qof", "copd.qof", "depression.qof")
 
-kable(DT)
+kable(head(df))
 
-write.csv(DT, "prev_est_wide.csv")
+
+
+write.csv(df, "prev_est_wide.csv")
 
 
 library(DT)
-datatable(DT,
+datatable(df,
           extensions = 'Buttons',
           options = list(
             dom = 'Bfrtip',
             buttons = c('copy', 'csv', 'excel', 'pdf', 'print')),
           caption = "Prevalence and QOF estimates")
 
-dt <- data.frame(DT)
+dt <- data.frame(df)
+
+dt <- dt %>%
+  select(bp1, bp.qof, depression, depression.qof, chd, chd.qof, copd, copd.qof) %>%
+  mutate(bp.diff = bp1- bp.qof, bp.ave = (bp1 + bp.qof)/2, 
+         dep.diff = depression - depression.qof, dep.ave = (depression + depression.qof)/2,
+         chd.diff = chd - chd.qof, chd.ave = (chd + chd.qof)/2,
+         copd.diff = copd - copd.qof, copd.ave = (copd + copd.qof)/2)
+
+summary(dt)
+
+## Bland Altman plots - plot differences between model and observed vs mean model + observed values
+
+q1 <-qplot(data = dt, 100 * bp.ave, 100 * bp.diff) + 
+  geom_hline(yintercept = 100 * mean(dt$bp.diff, na.rm = TRUE), colour = "red") + 
+  geom_hline(yintercept = 100 * (mean(dt$bp.diff, na.rm = TRUE) + 1.96 * sd(dt$bp.diff, na.rm = TRUE)), colour = "blue") + 
+  geom_hline(yintercept = 100 * (mean(dt$bp.diff, na.rm = TRUE) - 1.96 * sd(dt$bp.diff, na.rm = TRUE)), colour = "blue") 
+
+
+q2 <-qplot(data = dt, dep.ave, dep.diff) + geom_hline(yintercept = mean(dt$dep.diff, na.rm = TRUE), colour = "red") + geom_hline(yintercept = mean(dt$dep.diff, na.rm = TRUE) + 1.96 * sd(dt$dep.diff, na.rm = TRUE), colour = "blue") + geom_hline(yintercept = mean(dt$dep.diff, na.rm = TRUE) - 1.96 * sd(dt$dep.diff, na.rm = TRUE), colour = "blue")
+
+q3 <-qplot(data = dt, chd.ave, chd.diff) + geom_hline(yintercept = mean(dt$chd.diff, na.rm = TRUE), colour = "red")+ geom_hline(yintercept = mean(dt$chd.diff, na.rm = TRUE) + 1.96 * sd(dt$chd.diff, na.rm = TRUE), colour = "blue") + geom_hline(yintercept = mean(dt$chd.diff, na.rm = TRUE) - 1.96 * sd(dt$chd.diff, na.rm = TRUE), colour = "blue")
+
+q4 <-qplot(data = dt, copd.ave, copd.diff) + geom_hline(yintercept = mean(dt$chd.diff, na.rm = TRUE), colour = "red")+ geom_hline(yintercept = mean(dt$copd.diff, na.rm = TRUE) + 1.96 * sd(dt$copd.diff, na.rm = TRUE), colour = "blue") + geom_hline(yintercept = mean(dt$copd.diff, na.rm = TRUE) - 1.96 * sd(dt$copd.diff, na.rm = TRUE), colour = "blue")
+
+library(gridExtra)
+grid.arrange(q1, q2, q3, q4, ncol = 2 )
 
 ## Some plots and summaries
 
-source('~/Documents/R_projects/K_GP/panelcor.R')
-pairs(dt[,-1], lower.panel = panel.smooth, pch = 20)
+dev.off()
+require(corrplot)
+df <- data.frame(df)
+cor <- cor(df[,-1], use = 'complete.obs')
+corrplot(cor,  order = 'hclust', tl.cex = 0.5, tl.col = "black", addrect = 2, main = "Correlations between observed and predicted prevalences")
 
-qplot(data = filter(gp_all_prev, prevalence ==1| qof == 1 ), disease, value, geom = "boxplot", fill = qof) + coord_flip() + ggtitle("Boxplots of practice level estimated prevalence by disease") + xlab("Estimated prevalence %")
+qplot(data = filter(gp_all_prev, prevalence ==1), value, geom = "density", fill = disease) + xlab("Estimated prevalence %") + facet_wrap(~disease, scales = "free") + ggtitle("Density plots of estimated disease prevelance")
 
-qplot(data = filter(gp_all_prev, prevalence ==1), value, geom = "density", fill = disease, alpha = 0.4) + xlab("Estimated prevalence %")
+qplot(data = filter(gp_all_prev, prevalence ==1| qof ==1),  disease, log10(value), geom = "boxplot", fill = disease) + xlab("Estimated prevalence % (log scale)") + coord_flip() + ggtitle("Variation in practice level prevalence by disease")
+
+
 
 ## NAs
 
@@ -178,5 +225,8 @@ qplot(data = prevna, disease, meanNA *100, geom = "point")
 pracNA <- gp_all_prev %>% filter(is.na(value) & prevalence == 1) %>% select(practice_code, disease, value)
 head(pracNA)
 
+pracNA <- pracNA[-(231:232),]
+
 pracNAw <- spread(pracNA, disease, value)
 kable(pracNAw)
+
